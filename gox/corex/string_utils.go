e9bff5fe47c2a7d727c425a1fdb88b013e9f3b6c
wanruome <wanruome@126.com>
2024-01-24 15:25:22 +0800

    MatchStringEmptyPatternPass方法增加。

--- gox/corex/string_utils.go
+++ gox/corex/string_utils.go
@@ -61,3 +61,57 @@ func MatchStringCommon(pattern string, s string) bool {
 		return false
 	}
 }
+
+// 判断字符串是否匹配，支持正则表达式，支持开头结尾*通配符，支持严格匹配
+func MatchStringEmptyPatternPass(pattern string, s string) bool {
+	if len(pattern) <= 0 {
+		return true
+	}
+	if len(s) <= 0 {
+		return false
+	}
+	if strings.HasPrefix(pattern, "^") || strings.HasSuffix(pattern, "$") {
+		//使用正则校验
+		isMatch, err := regexp.MatchString(pattern, s)
+		if err != nil {
+			return false
+		} else {
+			return isMatch
+		}
+	} else if strings.HasPrefix(pattern, "*") && strings.HasSuffix(pattern, "*") {
+		//使用头尾通配符匹配模式
+		realPattern := pattern[1 : len(pattern)-1]
+		if len(realPattern) <= 0 {
+			return false
+		} else if strings.Contains(s, realPattern) {
+			return true
+		} else {
+			return false
+		}
+	} else if strings.HasSuffix(pattern, "*") {
+		//使用尾部通配符匹配模式
+		realPattern := pattern[0 : len(pattern)-1]
+		if len(realPattern) <= 0 {
+			return false
+		} else if strings.HasPrefix(s, realPattern) {
+			return true
+		} else {
+			return false
+		}
+	} else if strings.HasPrefix(pattern, "*") {
+		//使用头部通配符匹配模式
+		realPattern := pattern[1:len(pattern)]
+		if len(realPattern) <= 0 {
+			return false
+		} else if strings.HasSuffix(s, realPattern) {
+			return true
+		} else {
+			return false
+		}
+	} else if pattern == s {
+		//使用严格匹配模式
+		return true
+	} else {
+		return false
+	}
+}
