06dbebe5e0456377c13373bb19c007e2c30f9886
wanruome <wanruome@126.com>
2024-06-21 10:31:02 +0800

    Des加密逻辑加入。

--- gox/encryptx/zencrypt_test.go
+++ gox/encryptx/zencrypt_test.go
@@ -135,6 +135,36 @@ func TestAesCommon(t *testing.T) {
 	}
 }
 
+func TestDesCommon(t *testing.T) {
+
+	//time, _ := TimeParseByString(TIME_PATTERN_STANDARD, "2023-01-01 00:50:11")
+	var xHelper EncryptHelper
+	//xencrypt.
+	xHelper = &XDes{
+		ModeKey:        MODE_KEY_PEM,
+		ModeEncode:     MODE_ENCODE_BASE64,
+		ModePadding:    MODE_PADDING_PKCS7,
+		BlockSizeByKey: true,
+	}
+	keyStr, _ := xHelper.GenKeyIvString(8)
+	ivStr, _ := xHelper.GenKeyIvString(8)
+	print(keyStr)
+	xHelper.SetKeyString(keyStr)
+	xHelper.SetIVString(ivStr)
+	//xHelper.SetBlockSize(16)
+	origStr := "      中华人民共和国      " + GenRandomString(1024) + "      中华人民共和国      "
+	//origStr = ""
+	encStr, _ := xHelper.EncStringCBC(origStr)
+	fmt.Println(encStr)
+	decStr, _ := xHelper.DecStringCBC(encStr)
+	fmt.Println(decStr)
+	if origStr == decStr {
+		fmt.Println("加密解密验证通过")
+	} else {
+		fmt.Println("加密解密验证不通过通过")
+	}
+}
+
 func GenRandomString(size int) string {
 	tokenHelper := corex.TokenHelper{
 		TokenLen: 256,
