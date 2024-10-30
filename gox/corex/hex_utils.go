3dc9e1aa20ba6be6bb14bff37d7e6c5fde880ef2
wanruome <wanruome@126.com>
2024-10-30 13:36:22 +0800

    fix:增加hex判断。

---
+++ gox/corex/hex_utils.go
@@ -0,0 +1,54 @@
+/**
+ * @copyright www.ruomm.com
+ * @author 牛牛-wanruome@126.com
+ * @create 2024/10/28 16:33
+ * @version 1.0
+ */
+package corex
+
+import (
+	"errors"
+	"strings"
+)
+
+func IsHexString(str string, strictMatch bool) bool {
+	s := ""
+	if strings.HasPrefix(str, "0x") || strings.HasPrefix(str, "0X") {
+		s = str[2:]
+	} else {
+		s = str
+	}
+	if len(s) <= 0 {
+		return false
+	}
+	if strictMatch && len(s)%2 != 0 {
+		return false
+	}
+	for _, c := range s {
+		if (c < '0' || c > '9') && (c < 'a' || c > 'f') && (c < 'A' || c > 'F') {
+			return false
+		}
+	}
+	return true
+}
+
+func ToHexString(str string, strictMatch bool) (string, error) {
+	s := ""
+	if strings.HasPrefix(str, "0x") || strings.HasPrefix(str, "0X") {
+		s = str[2:]
+	} else {
+		s = str
+	}
+	if len(s) <= 0 {
+		return s, errors.New("invalid hex string，string is empty")
+	}
+	if strictMatch && len(s)%2 != 0 {
+		return "", errors.New("invalid hex string，string length is not even number")
+	}
+	for _, c := range s {
+		if (c < '0' || c > '9') && (c < 'a' || c > 'f') && (c < 'A' || c > 'F') {
+			return "", errors.New("invalid hex string，string contain no hex letter")
+		}
+	}
+	return s, nil
+}
